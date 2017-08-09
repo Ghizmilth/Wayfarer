e8bcc21d0f386414073f45c2053afc0a235e5546
Ghizmilth <hidanerferrer@gmail.com>
2017-08-09 15:11:30 -0700

    added bootstrap functionality

--- src/components/City.js
+++ src/components/City.js
@@ -1,26 +1,22 @@
-import React, {Component} 'react'
+import React, { Component } from "react";
 
 class City extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className="city-main">
-        
-
+        <div className="container">
+          <section className="row">
+            <article className="col-sm-4">
+              <h2>City List will be here</h2>
+            </article>
+            <article className="col-sm-4">
+              <h2>City info will be here</h2>
+            </article>
+          </section>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-export default City
+export default City;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,16 @@
-import React, {Component} from 'react'
-import Header from './Header'
-
+import React, { Component } from "react";
+import Header from "./Header";
+import City from "./City";
 
 class Main extends Component {
   render() {
     return (
       <div className="MainPage">
         <Header />
+        <City />
       </div>
     );
   }
 }
-
-
-
 
 export default Main;
