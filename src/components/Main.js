fb3820492cb1f48a1a1cd670ce8429a96e0c77d1
ryan-barrett <ascending62@gmail.com>
2017-08-09 14:48:53 -0700

    fix

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,12 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 //import Header from './Header'
-
 
 class Main extends Component {
   render() {
-    return (
-      <div className="MainPage">
-        This is a test
-      </div>
-    );
+    return <div className="MainPage">This is a test</div>;
   }
 }
 
-
-
+// test
 
 export default Main;
