f032f0fe82a88d96cd92fdc078f8397ea73b55aa
CalJersey <crippe@gmail.com>
2017-08-10 13:48:20 -0700

    iniitial commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 //import dependencies
-var express = require('express'),
-  mongoose = require('mongoose'),
-  bodyParser = require('body-parser'),
-  City = require('./models/city');
+var express = require("express"),
+  mongoose = require("mongoose"),
+  bodyParser = require("body-parser"),
+  Comment = require("./models/comment");
 
 //create instances
 var app = express(),
@@ -14,7 +14,7 @@ var app = express(),
 var port = process.env.API_PORT || 3001;
 
 //db config
-mongoose.connect('mongodb://localhost/mern-comment-box');
+mongoose.connect("mongodb://localhost/mern-comment-box");
 
 //config API to use bodyParser and look for JSON in req.body
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,29 +22,29 @@ app.use(bodyParser.json());
 
 //Prevent CORS errors
 app.use(function(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,HEAD,OPTIONS,POST,PUT,DELETE'
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
   );
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
   );
 
   //Remove caching
-  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader("Cache-Control", "no-cache");
   next();
 });
 
 //set route path and init API
-router.get('/', function(req, res) {
-  res.json({ message: 'API Initialized!' });
+router.get("/", function(req, res) {
+  res.json({ message: "API Initialized!" });
 });
 
 // delete all comments
-router.route('/nuke').get(function(req, res) {
+router.route("/nuke").get(function(req, res) {
   Comment.remove(function(err, succ) {
     res.json(succ);
   });
@@ -53,7 +53,7 @@ router.route('/nuke').get(function(req, res) {
 //add /comments route to our /api router here
 
 //use router config when we call /API
-app.use('/api', router);
+app.use("/api", router);
 
 //start server
 app.listen(port, function() {
