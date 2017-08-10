ea8cb5c44513db5de3cd8d0e1fc5b9cd69850a58
NrupM <maryenorthrup@gmail.com>
2017-08-10 16:40:32 -0700

    add user model

--- models/index.js
+++ models/index.js
@@ -3,3 +3,4 @@ mongoose.connect('mongodb://localhost/Wayfarer');
 
 module.exports.City = require('./city.js');
 module.exports.Post = require('./post.js');
+module.exports.User = require('./user.js');
---
+++ models/user.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username: String,
+  password: String
+  hometown: {
+    type: Schema.Types.ObjectId,
+    ref: 'City'
+  },
+  image: String
+});
+
+module.exports = mongoose.model('User', UserSchema);
