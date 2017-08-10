4a62be09351148f84d01c42d312dae3263631794
ryan-barrett <ascending62@gmail.com>
2017-08-10 09:11:26 -0700

    worked on PostBox delete

--- src/components/PostBox.js
+++ src/components/PostBox.js
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 import axios from "axios";
 
 class PostBox extends Component {
+  constructor(props){
+    super();
+    this.state = {data: [] };
+    this.handlePostDelete = this.handlePostDelete.bind(this);
+  }
+  handlePostDelete(id){
+    axios.delete(`${this.props.url}/${id}`)
+      .then(res => {
+        console.log('Post Deleted');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+}
+
   render() {
     return (
       <div className="post-box">
@@ -10,8 +25,13 @@ class PostBox extends Component {
           Add Post
         </button>
       </div>
+
+      <PostList
+        onPostDelete={ this.handlePostDelete }
+        data={ this.state.data } />  
     );
   }
+
 }
 
 export default PostBox;
