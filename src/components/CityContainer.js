397c375fec735a68644970c8d5d6802fda54d586
ryan-barrett <ascending62@gmail.com>
2017-08-11 16:11:31 -0700

    fix

--- src/components/CityContainer.js
+++ src/components/CityContainer.js
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
-import CityList from "./CityList";
+import PostBox from "./PostBox";
 import CityInfo from "./CityInfo";
 import "../MainStyle.css";
 
-class City extends Component {
+class CityContainer extends Component {
   render() {
     return (
       <div className="city-main">
         <div className="container">
           <section className="row">
-            <article className="col-sm-4">
-              <CityList />
-            </article>
+            <article className="col-sm-4" />
             <article className="col-sm-4">
               <CityInfo />
             </article>
           </section>
         </div>
+        <PostBox url="http://localhost:3001/api/posts" />
       </div>
     );
   }
 }
 
-export default City;
+export default CityContainer;
